21d6b145bb76ed165997571f5c8b8c55b43a1e8c
justin <justin.d.locke@gmail.com>
2020-08-21 15:49:02 -0400

    initial commit

---
+++ GC_Lab21_MovieRegistration/Controllers/MovieRegistrationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GC_Lab21_MovieRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GC_Lab21_MovieRegistration.Controllers
+{
+    public class MovieRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Movie());
+        }
+
+        public IActionResult MovieSummary(Movie movie)
+        {
+            string actorAll = movie.Actors[0];
+            string directorAll = movie.Directors[0];
+
+            movie.Actors = actorAll.Split(',');
+            movie.Directors = directorAll.Split(',');
+            
+            //checks that the user parameter fits the model aka not missing info/not bad format
+            if (ModelState.IsValid)
+            {
+                return View(movie); //if valid sends to user view
+            }
+            else
+            {
+                return View("Index", movie); //sends incorrect user back to form
+            }
+        }
+    }
+}
---
+++ GC_Lab21_MovieRegistration/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_Lab21_MovieRegistration.Models
+{
+    public class Movie
+    {
+        [Required]
+        public int ID { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string[] Actors { get; set; }
+        [Required]
+        public string[] Directors { get; set; }
+
+    }
+}
